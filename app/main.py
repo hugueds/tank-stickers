b9b64c02a991d8567d89a621f9d5e270428c1ea3
Hugo Peres <hugo-pdf@hotmail.com>
2021-01-25 12:36:57 -0300

    add analyse counter()

--- app/main.py
+++ app/main.py
@@ -25,6 +25,7 @@ while True:
         if controller.final_result:
             controller.set_state(AppState.SAVING_RESULTS)
         elif controller.read_plc.read_request:
+            print('New Job Request During Job')
             sleep(1)
             controller.set_state(AppState.WAITING_REQUEST)
         else:
