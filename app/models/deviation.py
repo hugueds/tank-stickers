6487af4eca7f1a91096ff8342e26451dbd9e9b00
Hugo Peres <hugo-pdf@hotmail.com>
2021-01-27 13:19:05 -0300

    update controller organisation

---
+++ app/models/deviation.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Deviation:
+    TANK_NOT_FOUND = 0
+    STICKER_POSITION = 2
+    STICKER_NOT_FOUND = 3
+    STICKER_QUANTITY = 4
+    DRAIN_POSITION = 7
+    STICKER_ANGLE = 8
+    STICKER_VALUE = 9
