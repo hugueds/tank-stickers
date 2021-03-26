e1b5a586c54129421c76f1e915bf94d3d1b95ef3
Hugo Peres <hugo-pdf@hotmail.com>
2021-03-26 07:27:02 -0300

    work update

--- app/classes/tank.py
+++ app/classes/tank.py
@@ -9,6 +9,7 @@ from .colors import *
 
 font = cv.FONT_HERSHEY_SIMPLEX
 
+
 class Tank:
 
     found = False
@@ -46,10 +47,8 @@ class Tank:
         self.area = config['area']
         self.threshold = config['threshold']
         width, height = config["size"]
-        self.min_width = width[0]
-        self.max_width = width[1]
-        self.min_height = height[0]
-        self.max_height = height[1]
+        self.min_width, self.max_width = width[0],  width[1]
+        self.min_height, self.max_height = height[0], height[1]
         self.radius = config['radius']
         self.min_dist = config['min_dist']
         self.params = config['params']
@@ -82,7 +81,7 @@ class Tank:
         else:
             if mode == 'circle':
                 self.find_convex(frame)
-            else: 
+            else:
                 self.find_in_circle(frame, _filter)
 
     def find_convex(self, frame):
@@ -91,42 +90,46 @@ class Tank:
         g_frame = self.__eliminate_non_roi(g_frame, ys, ye, xs, xe)
         g_frame = cv.GaussianBlur(g_frame, tuple(self.blur), 0)
         canny = cv.Canny(g_frame, self.canny[0], self.canny[1])
-        kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5,5))
+        kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
         canny = cv.dilate(canny, kernel)
-        contours, _ = cv.findContours(canny, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv.findContours(
+            canny, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         drawing = np.zeros((canny.shape[0], canny.shape[1], 3), dtype=np.uint8)
 
-        for i in range(len(contours)):            
+        for i in range(len(contours)):
             area = cv.contourArea(contours[i])
             if area > self.area[0] and area < self.area[1]:
                 cv.drawContours(drawing, contours, i, (255, 255, 255), 1)
 
         g_frame = cv.cvtColor(drawing, cv.COLOR_BGR2GRAY)
 
-        if self.debug_tank or True:
+        if self.debug_tank:
             cv.imshow('debug_tank', g_frame)
 
         self.circles = cv.HoughCircles(g_frame, cv.HOUGH_GRADIENT,
-                                        param1=self.params[0],
-                                        param2=self.params[1],
-                                        minDist=self.min_dist,
-                                        dp= self.radius[0],
-                                        minRadius=self.radius[1],
-                                        maxRadius=self.radius[2])
+                                       param1=self.params[0],
+                                       param2=self.params[1],
+                                       minDist=self.min_dist,
+                                       dp=self.radius[0],
+                                       minRadius=self.radius[1],
+                                       maxRadius=self.radius[2])
         self.found = False
         self.x, self.y, self.w, self.h = 0, 0, 0, 0
         if self.circles is not None:
             circles = np.uint16(np.around(self.circles))
             for x, y, r in circles[0, :]:
-                calc_x = int(x - r) if (x - r) > 0 and x < frame.shape[1] else 0
-                calc_y = int(y - r) if (y - r) > 0 and y < frame.shape[0] else 0
-                if (calc_x > 0 and calc_x < frame.shape[1]) and (calc_y > 0 and calc_y < frame.shape[0]):                    
+                calc_x = int(x - r) if (x -
+                                        r) > 0 and x < frame.shape[1] else 0
+                calc_y = int(y - r) if (y -
+                                        r) > 0 and y < frame.shape[0] else 0
+                if (calc_x > 0 and calc_x < frame.shape[1]) and (calc_y > 0 and calc_y < frame.shape[0]):
                     self.w, self.h = abs(2*r), abs(2*r)
                     self.x, self.y = calc_x, calc_y
                     self.found = True
-                    self.image = frame[self.y: self.y + self.h, self.x: self.x + self.w]
+                    self.image = frame[self.y: self.y +
+                                       self.h, self.x: self.x + self.w]
 
-    def find_in_circle(self, frame: np.ndarray, _filter, erode=True):
+    def find_in_circle(self, frame: np.ndarray, _filter, erode=False):
 
         g_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         ys, ye, xs, xe = self.get_roi(frame)
@@ -134,20 +137,26 @@ class Tank:
 
         if _filter == 'threshold':
             blur = cv.GaussianBlur(g_frame, tuple(self.blur), 0)
-            _, mask = cv.threshold(blur, self.threshold, 255, cv.THRESH_BINARY_INV)
+            _, mask = cv.threshold(blur, self.threshold,
+                                   255, cv.THRESH_BINARY_INV)
         elif _filter == 'adaptative':
-            mask = cv.adaptiveThreshold(g_frame,255,cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY,5,2)
+            mask = cv.adaptiveThreshold(
+                g_frame, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 5, 2)
         else:
             cut_frame = frame.copy()
             cut_frame = self.__eliminate_non_roi(cut_frame, ys, ye, xs, xe)
             if _filter == 'hsv':
                 cvt_frame = cv.cvtColor(cut_frame, cv.COLOR_BGR2HSV)
-                lower =  np.array( (self.table_hsv[0][0], self.table_hsv[0][1], self.table_hsv[0][2]), np.uint8)
-                higher = np.array( (self.table_hsv[1][0], self.table_hsv[1][1], self.table_hsv[1][2]), np.uint8)
+                lower = np.array(
+                    (self.table_hsv[0][0], self.table_hsv[0][1], self.table_hsv[0][2]), np.uint8)
+                higher = np.array(
+                    (self.table_hsv[1][0], self.table_hsv[1][1], self.table_hsv[1][2]), np.uint8)
             else:
                 cvt_frame = cv.cvtColor(cut_frame, cv.COLOR_RGB2LAB)
-                lower =  np.array( (self.table_hsv[0][0], self.table_hsv[0][1], self.table_hsv[0][2]), np.uint8)
-                higher = np.array( (self.table_hsv[1][0], self.table_hsv[1][1], self.table_hsv[1][2]), np.uint8)
+                lower = np.array(
+                    (self.table_hsv[0][0], self.table_hsv[0][1], self.table_hsv[0][2]), np.uint8)
+                higher = np.array(
+                    (self.table_hsv[1][0], self.table_hsv[1][1], self.table_hsv[1][2]), np.uint8)
             mask = cv.inRange(cvt_frame, lower, higher)
 
         if erode:
@@ -158,31 +167,36 @@ class Tank:
             cv.imshow('debug_tank', mask)
 
         self.circles = cv.HoughCircles(mask, cv.HOUGH_GRADIENT,
-                                        param1=self.params[0],
-                                        param2=self.params[1],
-                                        minDist=self.min_dist,
-                                        dp= self.radius[0],
-                                        minRadius=self.radius[1],
-                                        maxRadius=self.radius[2])
+                                       param1=self.params[0],
+                                       param2=self.params[1],
+                                       minDist=self.min_dist,
+                                       dp=self.radius[0],
+                                       minRadius=self.radius[1],
+                                       maxRadius=self.radius[2])
         self.found = False
         self.x, self.y, self.w, self.h = 0, 0, 0, 0
         if self.circles is not None:
             circles = np.uint16(np.around(self.circles))
             for x, y, r in circles[0, :]:
-                calc_x = int(x - r) if (x - r) > 0 and x < frame.shape[1] else 0
-                calc_y = int(y - r) if (y - r) > 0 and y < frame.shape[0] else 0
+                calc_x = int(x - r) if (x -
+                                        r) > 0 and x < frame.shape[1] else 0
+                calc_y = int(y - r) if (y -
+                                        r) > 0 and y < frame.shape[0] else 0
                 if (calc_x > 0 and calc_x < frame.shape[1]) and (calc_y > 0 and calc_y < frame.shape[0]):
                     self.w, self.h = 2 * abs(r), 2 * abs(r)
                     self.x, self.y = calc_x, calc_y
                     self.found = True
-                    self.image = frame[self.y: self.y + self.h, self.x: self.x + self.w]
+                    self.image = frame[self.y: self.y +
+                                       self.h, self.x: self.x + self.w]
 
     def find_up_camera(self, frame: np.ndarray, _filter='hsv'):
 
         hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
         blur = cv.blur(hsv, tuple(self.blur), 0)
-        lower =  np.array( (self.table_hsv[0][0], self.table_hsv[0][1], self.table_hsv[0][2]), np.uint8)
-        higher = np.array( (self.table_hsv[1][0], self.table_hsv[1][1], self.table_hsv[1][2]), np.uint8)
+        lower = np.array(
+            (self.table_hsv[0][0], self.table_hsv[0][1], self.table_hsv[0][2]), np.uint8)
+        higher = np.array(
+            (self.table_hsv[1][0], self.table_hsv[1][1], self.table_hsv[1][2]), np.uint8)
         mask = cv.inRange(blur, lower, higher)
 
         ys, ye, x_off_start, x_off_end = self.get_roi(frame)
@@ -194,7 +208,8 @@ class Tank:
         cam_config = self.config['camera']
         mid_x = frame.shape[1] // 2
 
-        x_center_offset = int(cam_config["center_x_offset"] * frame.shape[1] // 100)
+        x_center_offset = int(
+            cam_config["center_x_offset"] * frame.shape[1] // 100)
         vector_y = mask[:, mid_x + x_center_offset]
         roi_vector_y = vector_y[:]
         roi_vector_y = roi_vector_y[roi_vector_y == 0]
@@ -210,7 +225,8 @@ class Tank:
         self.y = np.where(vector_y == 0)[0][0]  # Get the first black pixel
 
         mid_y = frame.shape[0] // 2
-        y_center_offset = int(cam_config["center_y_offset"] * frame.shape[0] // 100)
+        y_center_offset = int(
+            cam_config["center_y_offset"] * frame.shape[0] // 100)
         vector_x = mask[mid_y + y_center_offset, :]
         roi_vector_x = vector_x[:]
         roi_vector_x = roi_vector_x[roi_vector_x == 0]
@@ -235,9 +251,11 @@ class Tank:
         _filter = self.sticker_filter
 
         if _filter == 'thresh':
-            _, mask = cv.threshold(g_frame, self.sticker_thresh, 255, cv.THRESH_BINARY)
+            _, mask = cv.threshold(
+                g_frame, self.sticker_thresh, 255, cv.THRESH_BINARY)
         elif _filter == 'canny':
-            mask = cv.Canny(g_frame, self.sticker_thresh, self.sticker_thresh * 1.5)
+            mask = cv.Canny(g_frame, self.sticker_thresh,
+                            self.sticker_thresh * 1.5)
         else:
             kernel = np.ones(self.sticker_kernel, np.uint8)
             if _filter == 'hsv':
@@ -263,7 +281,8 @@ class Tank:
     def append_stickers(self, mask, tank):
         camera = self.config['camera']['number']
         self.stickers = []
-        contour, _ = cv.findContours(mask, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+        contour, _ = cv.findContours(
+            mask, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
         for c in contour:
             area = cv.contourArea(c)
             cond = area >= self.sticker_area["min"]
@@ -283,7 +302,8 @@ class Tank:
                         sticker = Sticker(self.x + x, self.y + y, w, h, area)
                         sticker.image = tank[y: y + h, x: x + w]
                         sticker.set_relative(self)
-                        sticker.calc_quadrant(self.x, self.y, self.w, self.h, camera)
+                        sticker.calc_quadrant(
+                            self.x, self.y, self.w, self.h, camera)
                         self.stickers.append(sticker)
         self.quantity = len(self.stickers)
         if self.quantity == 1:
@@ -294,8 +314,9 @@ class Tank:
     def get_drain_ml(self, frame: np.ndarray, model: TFModel):
         self.drain_position = 0
         if self.found:
-            y_off_start, y_off_end, x_off_start, x_off_end = self.get_roi(frame)
-            croped_img = frame[y_off_start:y_off_end,x_off_start:x_off_end,:]
+            y_off_start, y_off_end, x_off_start, x_off_end = self.get_roi(
+                frame)
+            croped_img = frame[y_off_start:y_off_end, x_off_start:x_off_end, :]
             _, label = model.predict(croped_img)
             self.drain_position = int(label)
 
@@ -318,4 +339,3 @@ class Tank:
 
     def get_tank_image(self, frame: np.ndarray):
         return frame[self.y: self.y + self.h, self.x: self.x + self.w, :]
-
